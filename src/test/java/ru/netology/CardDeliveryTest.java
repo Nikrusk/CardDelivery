d0d151eae5e1edb81b16178a372119ec0746f194
NiK <nikrusk@list.ru>
2025-07-18 14:36:20 +0500

    HomeWork 2.0

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,17 +1,20 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    String planningDate = generateData(3);
 
     @BeforeEach
     void setUp() {
@@ -22,21 +25,17 @@ public class CardDeliveryTest {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    String planningDate = generateData(3);
-
     @Test
-    void ShouldCardDelivery() {
-        $("[data-test-id=city] input").setValue("Челябинск");
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=name] input").setValue("Иванов-Петров Николай");
+    void shouldCardDelivery() {
+        $("[data-test-id='city'] input").setValue("Челябинск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='name'] input").setValue("Иванов-Петров Николай");
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='phone'] input").setValue("+79999999999");
-        $("[data-test-id=agreement]").click();
-        $x(".//span[contains(text(),'Забронировать')]").click();
-        $("[data-test-id=notification]").should(visible, ofSeconds(15));
-        $("[data-test-id=notification]").$x(".//div[@class='notification__title']").should(text("Успешно!"));
-        $("[data-test-id=notification]").$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на " + planningDate));
-        $("[data-test-id=notification]").$x(".//button").click();
-        $("[data-test-id=notification]").should(hidden);
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
